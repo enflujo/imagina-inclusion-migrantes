acdb231d99214d77448969c15883c73e98911d2d
juanmi30 <j.manriquec@uniandes.edu.co>
2024-03-10 23:07:32 -0500

    avances map.ts

--- aplicaciones/www/fuente/mapa.ts
+++ aplicaciones/www/fuente/mapa.ts
@@ -1,3 +1,7 @@
+import mapboxgl from 'mapbox-gl';
+import type { GeoJSONSource, Map } from 'mapbox-gl';
+import type { Feature, FeatureCollection, Point } from 'geojson';
+
 export default async function mapa() {
   const datos = await fetch('/inclusion-municipios.json');
   console.log(datos);
