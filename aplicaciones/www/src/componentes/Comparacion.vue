5c07473cd77b782fe2ce726c2c124e71e33eb863
anattolia <anattolia@gmail.com>
2024-04-26 16:13:42 -0500

    ✨ Comparación entre barras

--- aplicaciones/www/src/componentes/Comparacion.vue
+++ aplicaciones/www/src/componentes/Comparacion.vue
@@ -23,36 +23,39 @@ const cerebroDatos = usarCerebroDatos();
         class="barra"
         :style="`top:${33 * i}px; width:${cerebroDatos.datos[lugar.id].valorIndice}%`"
       ></span>
-      <span
+      <div
         v-for="(lugar, i) in cerebroDatos.lugaresSeleccionados"
-        :key="`diferencia${i}`"
-        class="diferencia"
-        :style="`top:${i > 0 ? 33 * i - 10 : 0}px; left: ${cerebroDatos.lugaresSeleccionados.length > 1 && i > 0 ? (cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice < cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice ? cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice : cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice) : 0}%; width:  ${
-          cerebroDatos.lugaresSeleccionados.length > 1 && i > 0
-            ? (cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice >
-              cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
-                ? cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice -
-                  cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
-                : cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice -
-                  cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice
-              ).toFixed(2)
-            : 0
-        }%`"
-      ></span>
-      <span
-        v-for="(lugar, i) in cerebroDatos.lugaresSeleccionados"
-        :key="`diferencia-etiqueta${i}`"
-        class="diferencia-etiqueta"
-        :style="`top:${i > 0 ? 33 * i - 10 : 0}px; left: ${cerebroDatos.lugaresSeleccionados.length > 1 && i > 0 ? cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice : 0}%;`"
-        >{{
-          cerebroDatos.lugaresSeleccionados.length > 1 && i > 0
-            ? `dif: ${Math.abs(
-                cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice -
-                  cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
-              ).toFixed(2)}`
-            : ''
-        }}
-      </span>
+        class="diferencias"
+        :key="`diferencias${i}`"
+        :style="`display:${i > 0 ? 'visible' : 'none'};top:${i > 0 ? 33 * i - 15 : 0}px; left: ${cerebroDatos.lugaresSeleccionados.length > 1 && i > 0 ? (cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice < cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice ? cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice : cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice) : 0}%;`"
+      >
+        <span class="diferencia-limite"> </span>
+        <span
+          class="diferencia"
+          :style="`top:${i > 0 ? 33 * i - 10 : 0}px; width:  ${
+            cerebroDatos.lugaresSeleccionados.length > 1 && i > 0
+              ? (cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice >
+                cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
+                  ? cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice -
+                    cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
+                  : cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice -
+                    cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice
+                ).toFixed(2)
+              : 0
+          }%`"
+        ></span>
+        <span :key="`diferencia-etiqueta${i}`" class="diferencia-limite"> </span>
+        <span :key="`diferencia-etiqueta${i}`" class="diferencia-etiqueta"
+          >{{
+            cerebroDatos.lugaresSeleccionados.length > 1 && i > 0
+              ? `${Math.abs(
+                  cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice -
+                    cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
+                ).toFixed(2)}`
+              : ''
+          }}
+        </span>
+      </div>
       <span
         v-for="(lugar, i) in cerebroDatos.lugaresSeleccionados"
         :key="`lugar-barra${i}`"
@@ -106,25 +109,37 @@ const cerebroDatos = usarCerebroDatos();
     background-color: var(--azul);
     position: absolute;
   }
-
-  .diferencia {
-    height: 1px;
-    background-color: var(--negro);
-    display: block;
-    position: absolute;
-  }
-
-  .diferencia-etiqueta {
-    display: block;
-    position: absolute;
-    font-size: 0.7em;
-    color: grey;
-  }
-
   .etiqueta {
     position: absolute;
     margin-left: 0.2em;
     font-size: 0.8em;
+    color: var(--azul);
+  }
+
+  // Diferencias entre dos barras
+  .diferencias {
+    position: absolute;
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .diferencia {
+    height: 1px;
+    background-color: grey;
+    display: block;
+  }
+
+  .diferencia-limite {
+    height: 0.7em;
+    border-left: 1px grey solid;
+  }
+
+  .diferencia-etiqueta {
+    display: block;
+    font-size: 0.7em;
+    color: grey;
+    margin-inline-start: 0.2em;
   }
 }
 </style>
