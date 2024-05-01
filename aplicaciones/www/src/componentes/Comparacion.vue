04adfd587b12ebe33e7b36a203525c170c6bb29b
anattolia <anattolia@gmail.com>
2024-05-01 10:00:11 -0500

    🚧 Corregir comparaciones

--- aplicaciones/www/src/componentes/Comparacion.vue
+++ aplicaciones/www/src/componentes/Comparacion.vue
@@ -2,6 +2,14 @@
 import { usarCerebroDatos } from '@/cerebros/datos';
 
 const cerebroDatos = usarCerebroDatos();
+
+function buscarIndice(id: number) {
+  const municipio = cerebroDatos.datos.find((lugar) => lugar.id === id);
+
+  if (municipio) {
+    return municipio.valorIndice.toFixed(2);
+  }
+}
 </script>
 
 <template>
@@ -22,7 +30,7 @@ const cerebroDatos = usarCerebroDatos();
         v-for="(lugar, i) in cerebroDatos.lugaresSeleccionados"
         :key="`lugar-barra${i}`"
         class="barra"
-        :style="`top:${33 * i}px; width:${cerebroDatos.datos[lugar.id].valorIndice}%`"
+        :style="`top:${33 * i}px; width:${buscarIndice(lugar.id)}%`"
       ></span>
       <div
         v-for="(lugar, i) in cerebroDatos.lugaresSeleccionados"
@@ -35,12 +43,9 @@ const cerebroDatos = usarCerebroDatos();
           class="diferencia"
           :style="`top:${i > 0 ? 33 * i - 10 : 0}px; width:  ${
             cerebroDatos.lugaresSeleccionados.length > 1 && i > 0
-              ? (cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice >
-                cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
-                  ? cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice -
-                    cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
-                  : cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice -
-                    cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice
+              ? (buscarIndice(lugar.id) > cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
+                  ? buscarIndice(lugar.id) - cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
+                  : cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice - buscarIndice(lugar.id)
                 ).toFixed(2)
               : 0
           }%`"
@@ -48,10 +53,9 @@ const cerebroDatos = usarCerebroDatos();
         <span :key="`diferencia-etiqueta${i}`" class="diferencia-limite"> </span>
         <span :key="`diferencia-etiqueta${i}`" class="diferencia-etiqueta"
           >{{
-            cerebroDatos.lugaresSeleccionados.length > 1 && i > 0
+            cerebroDatos.lugaresSeleccionados.length >= 1 && i > 0 && cerebroDatos.lugaresSeleccionados[i - 1]
               ? `${Math.abs(
-                  cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i].id].valorIndice -
-                    cerebroDatos.datos[cerebroDatos.lugaresSeleccionados[i - 1].id].valorIndice
+                  buscarIndice(lugar.id) - buscarIndice(cerebroDatos.lugaresSeleccionados[i - 1]['id'])
                 ).toFixed(2)}`
               : ''
           }}
@@ -59,10 +63,10 @@ const cerebroDatos = usarCerebroDatos();
       </div>
       <span
         v-for="(lugar, i) in cerebroDatos.lugaresSeleccionados"
-        :key="`lugar-barra${i}`"
+        :key="`barra-etiqueta${i}`"
         class="etiqueta"
-        :style="`top:${33 * i}px; left: ${cerebroDatos.datos[lugar.id].valorIndice}%`"
-        >{{ cerebroDatos.datos[lugar.id].valorIndice.toFixed(2) }}</span
+        :style="`top:${33 * i}px; left: ${buscarIndice(lugar.id)}%`"
+        >{{ buscarIndice(lugar.id) }}</span
       >
     </div>
   </section>
