3ac80892bf27df89aae726d28cb04046cc4976e7
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-09-13 17:43:42 -0500

    🚧

--- aplicaciones/www/src/paginas/Escaleras.vue
+++ aplicaciones/www/src/paginas/Escaleras.vue
@@ -22,7 +22,7 @@ const totalV = 1468809;
  */
 
 const color = escalaColores(0, 100, '#963c3c', '#5a997e');
-console.log(color(82.65));
+
 const totalEmbarazadasV = 76599;
 const datosControlesV = [63306, 39883, 28246, 26243];
 const diferenciasV = datosControlesV.map((valor) => totalEmbarazadasV - valor);
@@ -30,173 +30,30 @@ const diferenciasPorcentajeV = diferenciasV.map((valor) => +((valor / totalEmbar
 const saltosDiferencias: number[] = [];
 const porcentajesV = datosControlesV.map((valor) => +((valor / totalEmbarazadasV) * 100).toFixed(2));
 const anchos = porcentajesV.map((valor, i) => [100 * (valor / 100), 100 * (diferenciasPorcentajeV[i] / 100)]);
-const datosCol = [100, 100, 100, 100, 77.01]; // estos son los últimos datos que mandó Arturo
 /** 
  * Según el Plan Decenal de Salud el 95 % de las mujeres gestantes debe tener cuatro o más controles prenatales.
 Según la meta de los Objetivos de Desarrollo Sostenible (ODS), para el 2030, el 93 % de las gestantes debe tener cuatro o más controles prenatales
  */
+const umbralPlanDecenal = 95;
+const umbralODS = 93;
 
 anchos.forEach((valores, i) => {
   const anterior = i === 0 ? 0 : anchos[i - 1][1];
   saltosDiferencias.push(+(valores[1] - anterior).toFixed(2));
 });
 
-const totalC = 26316000;
-const totalEmbarazadasC = totalC * 0.22;
-const datosControlesC = [
-  totalEmbarazadasC,
-  totalEmbarazadasC * 0.98,
-  totalEmbarazadasC * 0.93,
-  totalEmbarazadasC * 0.62,
-];
+const totalEmbarazadasC = 623715;
+const datosControlesC = [623715, 623715, 623715, 480334];
+const porcentajesC = datosControlesC.map((valor) => +((valor / totalEmbarazadasC) * 100).toFixed(2));
+const pasoX = 105 / (datosControlesC.length + 1);
 
-const porcentajesC = [100, 98, 93, 62];
-const contador = ref(0);
-const dims = { ancho: 0, alto: 0, izq: 0, der: 0 };
-let ctx: CanvasRenderingContext2D | null = null;
-const actualizarContador = (evento: any) => {
-  // console.log(evento);
-};
-const grafica = ref();
-const refLienzo: Ref<HTMLCanvasElement | null> = ref(null);
-let particulas: Particula[] = [];
+// const pasoX: Ref<number> = ref(0);
 
-console.log(porcentajesC, porcentajesV);
+// console.log(porcentajesC);
 
-onMounted(() => {
-  if (refLienzo.value) {
-    const lienzo = refLienzo.value;
-    if (!ctx) ctx = lienzo.getContext('2d') as CanvasRenderingContext2D;
-    escalar();
-    animacion(ctx, lienzo);
-  }
-
-  window.addEventListener('resize', escalar);
-  window.addEventListener('scroll', actualizarContador);
-});
-
-function crearParticulas() {
-  const numeroParticulas = 4000;
-  particulas = [];
-
-  const altoEscalon = 400;
-  const techoEscalones = dims.alto - altoEscalon;
-  const anchoEscalon = dims.ancho / 5;
-  let xMax = anchoEscalon;
-  let y1 = 0;
-  let pasan = numeroParticulas;
-
-  anchos.forEach(([pEscalera, pDiferencia], i) => {
-    if (i > 5) return;
-
-    const quedan = pasan * (pDiferencia / 100);
-    const y2 = altoEscalon * (pDiferencia / 100) - y1;
-    const velocidad = 0.4 + Math.random() * 0.3;
-
-    const xMax = anchoEscalon * (i + 1);
-
-    for (let i = 0; i < quedan; i++) {
-      const x = Math.random() * xMax;
-      const y = y1 + techoEscalones + Math.random() * y2;
-      particulas.push(new Particula(x, y, velocidad, [0, 0], [xMax, 0], true));
-    }
-
-    y1 += y2;
-
-    if (i < porcentajesV.length - 1) {
-      pasan -= quedan;
-    } else {
-      console.log('quedaron', pasan, particulas.length);
-    }
-
-    // if (i > -1) {
-    //   const anchoDif = altoEscalon * (diferenciasPorcentajeV[i] / 100);
-    //   const anchoEscalera = altoEscalon * (porcentaje / 100);
-    //   console.log(anchoDif, anchoEscalera, anchoDif + anchoEscalera);
-    //   xMax += anchoDif + anchoEscalera;
-    // }
-  });
-
-  for (let i = 0; i < pasan * 4; i++) {
-    const x = Math.random() * (anchoEscalon * 5);
-    const y2 = altoEscalon * (100 / 100) - y1;
-    const y = y1 + techoEscalones + Math.random() * y2;
-    particulas.push(new Particula(x, y, 0.4 + Math.random() * 0.3, [0, 0], [anchoEscalon * 5, 0], false));
-  }
-
-  // const corte1 = porcentajesV[0];
-  // const pasanCorte1 = numeroParticulas * (corte1 / 100);
-  // const quedanCorte1 = numeroParticulas - pasanCorte1;
-
-  // let y2 = altoEscalon * (diferenciasPorcentajeV[0] / 100);
-  // let velocidad = Math.random() * 0.1;
-
-  // for (let i = 0; i < quedanCorte1; i++) {
-  //   const x = Math.random() * xMax;
-  //   const y = y1 + techoEscalones + Math.random() * y2;
-  //   particulas.push(new Particula(x, y, velocidad, [0, 0], [xMax, 0]));
-  // }
-
-  // xMax = anchoEscalon * 2;
-  // y1 = y2;
-  // y2 = altoEscalon * (diferenciasPorcentajeV[1] / 100) - y2;
-  // velocidad = Math.random() * 1.3;
-
-  // const corte2 = porcentajesV[1];
-  // const pasanCorte2 = numeroParticulas * (corte2 / 100);
-  // const quedanCorte2 = numeroParticulas - pasanCorte2;
-
-  // for (let i = 0; i < quedanCorte2; i++) {
-  //   const x = Math.random() * xMax;
-  //   const y = y1 + techoEscalones + Math.random() * y2;
-  //   particulas.push(new Particula(x, y, velocidad, [0, 0], [xMax, 0]));
-  // }
-
-  // for (let i = 0; i < numeroParticulas; i++) {
-  //   const x = dims.izq;
-  //   const y = 100 + Math.random() * 400;
-  //   particulas.push(new Particula(x, y, 1.2 + Math.random(), [dims.der, 400]));
-  // }
-}
-
-function animacion(ctx: CanvasRenderingContext2D, lienzo: HTMLCanvasElement) {
-  function ciclo() {
-    ctx.clearRect(0, 0, lienzo.width, lienzo.height);
-
-    particulas.forEach((particula) => {
-      particula.mover();
-      ctx.fillRect(particula.x, particula.y, 1.5, 1.5);
-    });
-    reloj = requestAnimationFrame(ciclo);
-  }
-  reloj = requestAnimationFrame(ciclo);
-}
-
-onUnmounted(() => {
-  cancelAnimationFrame(reloj);
-  window.removeEventListener('resize', escalar);
-  window.removeEventListener('scroll', actualizarContador);
-});
-
-function numeroConteo(valor: number) {
-  return new Intl.NumberFormat('en-US').format(valor);
-}
-
-function escalar() {
-  if (refLienzo.value && grafica.value) {
-    dims.ancho = refLienzo.value.width = (grafica.value.clientWidth * 0.9) | 0;
-    dims.alto = refLienzo.value.height = grafica.value.clientHeight;
-    const margen = dims.ancho / 2;
-    dims.izq = margen;
-    dims.der = dims.ancho - margen;
-
-    if (ctx) {
-      ctx.fillStyle = '#498E75';
-    }
-
-    crearParticulas();
-  }
-}
+// function escalar() {
+//   pasoX.value = datosControlesC
+// }
 </script>
 
 <template>
@@ -232,7 +89,30 @@ function escalar() {
   </header>
 
   <div id="grafica" ref="grafica">
-    <span class="escalon escalon1" :style="`color:${color(100)}`"
+    <span id="umbralDecenal" :style="`bottom:${umbralPlanDecenal}%`"></span>
+
+    <div class="seccionEscalones">
+      <span :style="`left: 0%; top: ${0}%; width: ${pasoX}%;height:100%`" class="escalonColombiana"> </span>
+      <span
+        v-for="(valor, i) in porcentajesC"
+        :ref="`porcentajeCol${i}`"
+        :style="`left: ${(i + 1) * pasoX}%; top: ${100 - valor}%; width: ${pasoX}%;height: ${valor}%`"
+        class="escalonColombiana"
+      >
+      </span>
+
+      <span :style="`left: 0%; top: ${0}%; width: ${pasoX}%;height:100%`" class="escalonVenezolana"> </span>
+
+      <span
+        v-for="(valor, i) in porcentajesV"
+        :ref="`porcentajeCol${i}`"
+        :style="`left: ${(i + 1) * pasoX}%; top: ${100 - valor}%; width: ${pasoX}%;height: ${valor}%`"
+        class="escalonVenezolana"
+      >
+      </span>
+    </div>
+
+    <!-- <span class="escalon escalon1" :style="`color:${color(100)}`"
       >100% <span class="mensaje">Mujeres Venezolanas embarazadas</span></span
     >
     <span class="diferencia diferencia1">
@@ -271,9 +151,9 @@ function escalar() {
       :style="`font-size:${3 * (porcentajesV[3] / 100)}vw; color:${color(porcentajesV[3])}`"
       >{{ porcentajesV[3] }}%
       <span class="mensaje">Hicieron sus 4 controles prenatales</span>
-    </span>
+    </span> -->
 
-    <canvas id="lienzo" ref="refLienzo"></canvas>
+    <!-- <canvas id="lienzo" ref="refLienzo"></canvas> -->
   </div>
 
   <!-- <div class="columnas">
@@ -327,6 +207,7 @@ function escalar() {
 $colorPositivo: rgb(13, 141, 130);
 $colorPositivo2: rgb(13, 141, 130);
 $colorNegativo: rgb(210, 87, 87);
+$colorColombiana: rgb(130, 13, 141);
 $altoTotal: 400px;
 $anchoEscalon: calc(90vw / 5);
 $alto2: calc($altoTotal * 0.8265);
@@ -337,6 +218,29 @@ $alto4: calc($altoTotal * 0.3789);
 $diferencia3: calc($alto3 - $alto4);
 $alto5: calc($altoTotal * 0.3426);
 $diferencia4: calc($alto4 - $alto5);
+
+#umbralDecenal {
+  position: absolute;
+  width: calc(100% - 10%);
+  border-top: 5px solid $colorPositivo;
+}
+
+.seccionEscalones {
+  position: relative;
+  width: 90%;
+}
+
+.escalonColombiana {
+  // border: 2px solid $colorColombiana;
+  position: absolute;
+  background-color: rgba($colorColombiana, 0.4);
+}
+
+.escalonVenezolana {
+  // border: 2px solid $colorPositivo2;
+  position: absolute;
+  background-color: rgba($colorPositivo2, 0.4);
+}
 
 .bajo {
   color: $colorNegativo;
