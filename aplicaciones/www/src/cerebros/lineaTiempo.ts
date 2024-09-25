6f6ae42be713bde311b28a6992c2324d2dd5ff33
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-09-25 13:54:22 -0500

    📝 base para datos de linea

---
+++ aplicaciones/www/src/cerebros/lineaTiempo.ts
@@ -0,0 +1,17 @@
+type MomentoLinea = {
+  año: number;
+  numero: number | null;
+  texto: string;
+};
+export const datosLinea: MomentoLinea[] = [
+  {
+    año: 2008,
+    numero: null,
+    texto: 'Te econo....',
+  },
+  {
+    año: 2014,
+    numero: 23573,
+    texto: 'Only 11,429 are regular migrants.',
+  },
+];
