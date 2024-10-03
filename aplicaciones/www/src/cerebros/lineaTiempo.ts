bc611b2b0f2bc0739772510d02726317a90062b6
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-10-03 11:52:33 -0500

    👷 v1 línea de tiempo

--- aplicaciones/www/src/cerebros/lineaTiempo.ts
+++ aplicaciones/www/src/cerebros/lineaTiempo.ts
@@ -1,40 +1,37 @@
-type MomentoLinea = {
-  año: number;
-  numero: number | null;
-  texto: string;
-};
+import type { MomentoLinea } from '@/tipos';
 
 export const datosLinea: MomentoLinea[] = [
   {
     año: 2008,
-    numero: null,
-    texto: 'Comienza la crisis económica en Venezuela.',
+    numero: 21142,
+    texto: 'comienza la crisis económica en Venezuela.',
+    clase: 'extremo',
   },
   {
     año: 2014,
     numero: 23573,
-    texto: 'Solamente 11,429 migrantes están regularizados.',
+    texto: 'solamente 11,429 migrantes están regularizados.',
   },
   {
     año: 2015,
     numero: 31471,
-    texto: 'Más de 22,000 colombianos que viven en Venezuela regresan a Colombia.',
+    texto: 'más de 22,000 colombianos que viven en Venezuela regresan a Colombia.',
   },
   {
     año: 2016,
     numero: 57747,
-    texto: 'En promedio, 50,000 Venezolanos transitan entre Colombia y Venezuela.',
+    texto: 'en promedio, 50,000 Venezolanos transitan entre Colombia y Venezuela.',
   },
   {
     año: 2017,
     numero: 403707,
     texto:
-      'El gobierno Colombiano crea el PEP, con este permiso, 68,884 Venezolanos legalizan su estancia en Colombia.',
+      'el gobierno Colombiano crea el PEP, con este permiso, 68,884 Venezolanos legalizan su estancia en Colombia.',
   },
   {
     año: 2018,
     numero: 1072432,
-    texto: 'El gobierno Colombiano presenta el CONPES para atender la crisis migratoria.',
+    texto: 'el gobierno Colombiano presenta el CONPES para atender la crisis migratoria.',
   },
   {
     año: 2019,
@@ -45,16 +42,32 @@ export const datosLinea: MomentoLinea[] = [
     año: 2020,
     numero: 1715831,
     texto:
-      'La frontera cierra por la pandemia del COVID19 provocando un aumento en la migración irregular. El número de Venezolanos en Colombia desciende 6.2% - de 1,809,872 en Marzo a 1,715,831 en Septiembre.',
+      'la frontera cierra por la pandemia del COVID19 provocando un aumento en la migración irregular. El número de Venezolanos en Colombia desciende 6.2% - de 1,809,872 en Marzo a 1,715,831 en Septiembre.',
   },
   {
     año: 2021,
-    numero: null,
+    numero: 1842390,
     texto: '...',
+    clase: 'acento',
   },
   {
     año: 2022,
     numero: 2477588,
-    texto: 'En promedio, 50,000 Venezolanos transitan entre Colombia y Venezuela.',
+    texto: 'en promedio, 50,000 Venezolanos transitan entre Colombia y Venezuela.',
+    clase: 'extremo',
   },
 ];
+
+export const fechaInicial = datosLinea[0].año;
+export const fechaFinal = datosLinea[datosLinea.length - 1].año;
+
+let min = Infinity;
+let max = 0;
+
+datosLinea.forEach((punto) => {
+  if (min > punto.numero) min = punto.numero;
+  if (max < punto.numero) max = punto.numero;
+});
+
+export const numeroMin = min;
+export const numeroMax = max;
--- aplicaciones/www/src/componentes/LineaTiempo.vue
+++ aplicaciones/www/src/componentes/LineaTiempo.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
-import { datosLinea } from '@/cerebros/lineaTiempo';
+import { datosLinea, fechaFinal, fechaInicial, numeroMax, numeroMin } from '@/cerebros/lineaTiempo';
+import type { MomentoLinea } from '@/tipos';
+import { formatoNumero } from '@/utilidades/ayudas';
+import { convertirEscala } from '@enflujo/alquimia';
 import { onMounted, onUnmounted, ref, type Ref } from 'vue';
 
 const grafica: Ref<(HTMLElement & SVGElement) | undefined> = ref();
+const margenX = 20;
+const margenY = 20;
+const dims = ref({ anchoTotal: 0, altoTotal: 0, ancho: 0, alto: 0, techo: 0, piso: 0 });
+const infoVisible = ref(false);
+const infoX = ref(0);
+const infoY = ref(0);
+const textoInfo = ref('');
+const textoAño = ref('');
+const textoNumero = ref('');
+const posY = (valor: number) => convertirEscala(valor, numeroMin, numeroMax, dims.value.techo, dims.value.piso);
+const posX = (valor: number) => convertirEscala(valor, fechaInicial, fechaFinal, margenX, dims.value.ancho);
+const radio = (valor: number) => convertirEscala(valor, numeroMin, numeroMax, 5, 25);
 
 onMounted(() => {
   escalar();
@@ -19,19 +34,125 @@ function escalar() {
   const contenedor = svg.parentElement;
 
   if (contenedor) {
-    svg.setAttribute('width', `${contenedor.clientWidth}`);
-    svg.setAttribute('height', `${svg.clientHeight}`);
+    const anchoTotal = contenedor.clientWidth;
+    const altoTotal = svg.clientHeight;
+    const ancho = anchoTotal - margenX * 2;
+    const alto = altoTotal - margenY * 2;
+    svg.setAttribute('width', `${anchoTotal}`);
+    svg.setAttribute('height', `${altoTotal}`);
+    Object.assign(dims.value, {
+      ancho,
+      alto,
+      piso: alto - margenY,
+      techo: margenY,
+    });
   }
 }
+
+function mostrarInfo(punto: MomentoLinea) {
+  infoVisible.value = true;
+  textoInfo.value = punto.texto;
+  textoAño.value = `${punto.año}`;
+  textoNumero.value = formatoNumero.format(punto.numero);
+}
+
+function esconderInfo() {
+  infoVisible.value = false;
+}
+
+function actualizarPosicion(evento: MouseEventInit) {
+  const { clientX, clientY } = evento;
+  if (clientX && clientY) {
+    infoX.value = clientX;
+    infoY.value = clientY;
+  }
+}
+
 console.log(datosLinea);
 </script>
 
 <template>
-  <svg class="lineaTiempo" ref="grafica"></svg>
+  <svg class="lineaTiempo" ref="grafica" @mousemove="actualizarPosicion">
+    <g
+      v-for="punto in datosLinea"
+      class="punto"
+      :style="{ transform: `translate(${posX(punto.año)}px, ${posY(punto.numero)}px)` }"
+      @mouseenter="mostrarInfo(punto)"
+      @mouseleave="esconderInfo"
+    >
+      <circle :class="`circulo ${punto.clase || ''}`" x="0" y="0" :r="radio(punto.numero)" />
+      <text class="año" x="-12" :y="`-${radio(punto.numero) + 3}`">{{ punto.año }}</text>
+    </g>
+  </svg>
+
+  <div :class="`info${infoVisible ? ' visible' : ''}`" :style="{ left: `${infoX}px`, top: `${infoY}px` }">
+    <p class="textoInfo">En {{ textoAño }} {{ textoInfo }}</p>
+    <p class="textoAño">
+      <span class="resaltar">{{ textoNumero }}</span> Venezolanos viven en Colombia
+    </p>
+  </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .lineaTiempo {
-  height: 50vh;
+  height: 35vh;
+}
+
+.punto {
+  cursor: pointer;
+}
+
+.circulo {
+  fill: rgb(202, 96, 223);
+  filter: drop-shadow(0 0 0.55rem rgba(0, 0, 0, 0.2));
+
+  &.acento {
+    stroke: rgb(1, 2, 2);
+    stroke-width: 0.5;
+    fill: rgb(70, 202, 129);
+    // stroke-dasharray: 3;
+    animation: 2s infinite alternate acento;
+  }
+
+  &.extremo {
+    stroke: rgb(13, 15, 14);
+    stroke-width: 2;
+    stroke-dasharray: 2;
+  }
+}
+.año {
+  font-size: 0.65em;
+  // transform: translate(-1em, 40px);
+}
+
+.resaltar {
+  font-weight: bold;
+}
+
+.info {
+  width: 200px;
+  min-height: 100px;
+  border: 1px solid;
+  background-color: rgba(241, 235, 235, 0.9);
+  position: absolute;
+  transform: translate(15px, -30%);
+  display: none;
+  font-size: 0.75em;
+  padding: 0.5em;
+  z-index: 999;
+
+  &.visible {
+    display: block;
+  }
+}
+
+@keyframes acento {
+  from {
+    filter: drop-shadow(0 0 0.75rem rgba(0, 0, 0, 0.5));
+  }
+
+  to {
+    filter: drop-shadow(0 0 0.75rem rgb(0, 0, 0, 0.8));
+  }
 }
 </style>
